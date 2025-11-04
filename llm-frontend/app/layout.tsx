2ace0fd70d478ba8d55ec38b831689191d6bbbfa
annaandmandy <annaandtommy1004@gmail.com>
2025-11-04 00:07:37 -0500

    add vercel analytics

--- llm-frontend/app/layout.tsx
+++ llm-frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "LLM Brand Experiment",
@@ -21,6 +22,7 @@ t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "u0mj14g341");`}
         </Script>
+        <Analytics />
         {children}
       </body>
     </html>
